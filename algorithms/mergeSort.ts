5c45940e713993e74e30dee881d7eaedc4cdb842
asobirov <asobirov357@gmail.com>
2021-11-26 06:20:12 +0000

    Minor fix

--- algorithms/mergeSort.ts
+++ algorithms/mergeSort.ts
@@ -9,5 +9,17 @@ export const mergeSort = (arr: number[]): number[] => {
 }
 
 const mergeHalves = (left: number[], right: number[]): number[] => {
-    
+    const sortedArr: number[] = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            sortedArr.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            sortedArr.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+    return sortedArr.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
